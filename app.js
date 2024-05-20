57b00b15909671506e01dc36c0bf87ae25161792
oripovnodirbek <nodirbekoripov1333@gmail.com>
2024-05-20 19:16:43 +0500

    3dars 2-amaliyot

---
+++ app.js
@@ -0,0 +1,50 @@
+const body = document.querySelector('body')
+const container = document.querySelector('.container')
+const colortext = document.querySelector('.color-text ')
+
+const values = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+]
+
+function getGradient () {
+    let color = '#'
+    for(let i=0; i<6; i++) {
+        const randomNumber = Math.trunc(Math.random() * values.length)
+        color += values[randomNumber]
+    }
+    return color
+}
+
+
+
+
+function setGradient() {
+    const color1 = getGradient()
+    const color2 = getGradient()
+    const randomDag = Math.trunc(Math.random() * 360)
+    const bgColor = `linear-gradient(
+        ${randomDag}deg,
+        ${color1},
+        ${color1}
+    )`
+    body.style.background =bgColor
+    colortext.textContent = bgColor
+}
+
+setGradient()
+container.addEventListener('click', setGradient)
